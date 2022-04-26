14ff704ecdc4ff809a8a204f4df5108f72459fca
Moises Sanchez <moy5921@gmail.com>
2022-04-26 14:24:49 -0500

    creando clase spiderman

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const {crearArchivo} = require('./helpers/multiplicar');
 const base = 7;
 console.clear();
 
-crearArchivo(base);
+console.log(process.argv);
+
+const [,,tercerArgumento = 'base= 5'] = process.argv;
+console.log(tercerArgumento)
+
+// crearArchivo(base)
+// .then(nombreArchivo => console.log(nombreArchivo,'creado'))
+// .catch(err => console.log(err));
